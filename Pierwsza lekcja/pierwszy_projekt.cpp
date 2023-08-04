cde431e69ab55e34444f50b06a7cfa467d7bc557
avolfer <jakubstuba2004@gmail.com>
2023-08-04 17:39:41 +0200

    .,..

--- Pierwsza lekcja/pierwszy_projekt.cpp
+++ Pierwsza lekcja/pierwszy_projekt.cpp
@@ -4,6 +4,10 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world" << endl;
+    string imie;
+    cout << "Jak masz na imie: ";
+    cin >> imie;
+
+    cout << "Hello " << imie << endl;
     return 0;
 }
