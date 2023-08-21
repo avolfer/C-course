a19818053f63f7514fd650ad997314bca0601617
avolfer <jakubstuba2004@gmail.com>
2023-08-21 12:57:39 +0200

    ...

---
+++ first_program/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    int favorite_number;
+    cout << "Enter your favorite number between 1 and 100: ";
+
+    cin >> favorite_number;
+
+    cout << "good choise!" << endl;
+
+    return 0;
+}
